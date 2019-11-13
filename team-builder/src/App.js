5ad0bb67d32c2bc36508dfc1b5153a31f85474e5
jordan <jordan@jordans-MacBook-Air.local>
2019-11-13 20:56:55 +0700

    added form close to mvp

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,13 +1,56 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
+  const [members, setMembers]= useState({
+    members: "",
+    email:"",
+    role:""
+  });
+
+  const FormChange = e => {
+    setMembers({
+      ...members,
+      [e.target.member]: e.target.value
+    });
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
   return (
     <div className="App">
       <h1>Welcome to AE Skateshop</h1>
-      
+      <form onSubmit={handleSubmit}>
+        <label>
+          Members:
+          <input
+            type="text"
+            name="member"
+            placeholder="Enter Member Name "
+            onChange={FormChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            placeholder="Enter Email"
+            onChange={FormChange}
+          />
+        </label>
+        <label>
+          Role:
+          <input
+            type="text"
+            name="role"
+            placeholder="Enter Role"
+            onChange={FormChange}
+          />
+        </label>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
